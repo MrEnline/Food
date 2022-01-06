45cbe1aada2d25a10f77344ab43a5958f6e7d02a
Kuznetsov Sergey <mr.enline@gmail.com>
2022-01-06 18:38:24 +0300

    Сделан калькулятор расчета калорий

--- js/script.js
+++ js/script.js
@@ -603,6 +603,73 @@ window.addEventListener('DOMContentLoaded', () => {
     // });
 
 
+    //Calculate calories
+    const result = document.querySelector('.calculating__result span');
+    let sex = 'female', 
+        height, weight, age,
+        ratio = 1.375;
+
+    function calcTotal() {
+        if (!height || !weight || !age) {
+            result.textContent = '___';
+            return;
+        }
+        if (sex == 'female') {
+            result.textContent = Math.round((447.6 + (9.2 * weight) + (3.1 * height) - (4.3 * age)) * ratio);
+        }else {
+            result.textContent = Math.round((88.36 + (13.4 * weight) + (4.8 * height) - (5.7 * age)) * ratio);
+        }
+    }
+    
+    calcTotal();
+
+    function getStaticInformation(parentSelector, activeClass) {
+        //получим все элементы div с определенным классом в родителе
+        const elements = document.querySelectorAll(`${parentSelector} div`);
+        //навесим на каждый элемент обработчик события
+        elements.forEach(element => {
+            element.addEventListener('click', (e) => {
+                if (e.target.getAttribute('data-ratio')) {
+                    ratio = +e.target.getAttribute('data-ratio');
+                }else {
+                    sex = e.target.getAttribute('id');
+                }
+                elements.forEach(el => {
+                    el.classList.remove(activeClass);
+                })
+                e.target.classList.add(activeClass);
+                calcTotal();
+            })
+        })
+    }
+
+    getStaticInformation('#gender', 'calculating__choose-item_active');
+    getStaticInformation('.calculating__choose_big', 'calculating__choose-item_active');
+
+    function getDynamicInformation(id) {
+        const element = document.querySelector(id);
+        element.addEventListener('input', (e) => {
+            switch (e.target.getAttribute('id')) {
+                case "height":
+                    height = +e.target.value;
+                    break;
+                case "weight":
+                    weight = +e.target.value;
+                    break; 
+                case "age":
+                    age = +e.target.value;
+                    break;       
+            }
+            calcTotal();
+        })
+    }
+
+    getDynamicInformation('#height');
+    getDynamicInformation('#weight');
+    getDynamicInformation('#age');
+
+
+    
 
     // //тестовый GET-запрос на сервер с Json-данными через промисы
     // fetch('https://jsonplaceholder.typicode.com/todos/1')
