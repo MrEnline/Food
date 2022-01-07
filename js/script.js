d7a94f2bd5b26cab3fb06a3967d0342ba22c9091
Kuznetsov Sergey <mr.enline@gmail.com>
2022-01-08 00:35:01 +0300

    Рефакторинг проекта - добавление экспортов и импортов функциям. Использование webpack при работе

--- js/script.js
+++ js/script.js
@@ -1,21 +1,33 @@
 'use strict'
+import  tabs from './modules/tabs';
+import  cards from './modules/cards';
+import  calc from './modules/calc';
+import  forms from './modules/forms';
+import  modal from './modules/modal';
+import  slider from './modules/slider';
+import  timer from './modules/timer';
+import {openModal} from './modules/modal';
 
 window.addEventListener('DOMContentLoaded', () => {
-    const tabs = require('./modules/tabs'),
-          cards = require('./modules/cards'),
-          calc = require('./modules/calc'),
-          forms = require('./modules/forms'),
-          modal = require('./modules/modal'),
-          slider = require('./modules/slider'),
-          timer = require('./modules/timer');
     
-    tabs();
+    const modalTimerId = setTimeout(()=> openModal('.modal', modalTimerId), 300000);
+
+    tabs('.tabheader__item', '.tabcontent', '.tabheader__items', 'tabheader__item_active');
     cards();
     calc();
-    forms();
-    modal();
-    slider();
-    timer();      
+    forms('form', modalTimerId);
+    modal('[data-modal]', '.modal', modalTimerId);
+    timer('.timer', '2022-06-01');      
+    slider({
+        container: '.offer__slider',
+        slide: '.offer__slide',
+        nextArrow: '.offer__slider-next',
+        prevArrow: '.offer__slider-prev',
+        totalCounter: '#total',
+        currentCounter: '#current',
+        wrapper: '.offer__slider-wrapper',
+        field: '.offer__slider-inner'
+    });
 
     // //тестовый GET-запрос на сервер с Json-данными через промисы
     // fetch('https://jsonplaceholder.typicode.com/todos/1')
